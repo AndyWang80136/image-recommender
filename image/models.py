2d2a3d7815f54ee7177f5e0407ced1be7ae1c077
AndyWang(Wang Chun-Hao) <andy80136@gmail.com>
2024-07-04 16:22:09 +0800

    feat: setup project and app

---
+++ image/models.py
@@ -0,0 +1,29 @@
+import uuid
+from pathlib import Path
+
+from django.db import models
+
+
+# Create your models here.
+def custom_image_path(instance, filename):
+    image_id = str(instance.id)
+    timestamp = str(instance.created_date.timestamp())
+    extension = Path(filename).suffix
+    return f'images/{image_id}-{timestamp}{extension}'
+
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    modified_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=False, upload_to=custom_image_path)
+    source = models.CharField(max_length=200, default='example', blank=False)
+
+    def __str__(self):
+        return f'{self.id}'
+    
+    def to_query(self):
+        return {
+            'image_id': str(self.id),
+            'image_url': self.image.url
+        } 
